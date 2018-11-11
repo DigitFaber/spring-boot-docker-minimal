4b6cbec01bb0f2e010913ee0ba6e0ae30080b5c6
Tomasz Klaja <digitfaber@gmail.com>
2018-11-11 13:59:54 +0100

    added early version of the project

---
+++ src/main/java/com/digitfaber/spring_boot_docker_minimal/hello_world/HelloWorldApplication.java
@@ -0,0 +1,20 @@
+package com.digitfaber.spring_boot_docker_minimal.hello_world;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class HelloWorldApplication {
+
+    @RequestMapping("/")
+    public String home() {
+        return "Hello Docker Spring Boot World!";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloWorldApplication.class, args);
+    }
+}
